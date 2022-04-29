e9366ab976bed9e44fc980277dede3d98d5701e3
Xavier McClurkin <104314713+gitignore-xav@users.noreply.github.com>
2022-04-29 12:35:24 +0000

    Added more preprocessors

--- .ccls-cache/@home@runner@E/main.c
+++ .ccls-cache/@home@runner@E/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include "e.h"
 
-e number = 0;
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+E EE EEeee Eeeee EEeeee EEe
+	e EEeee "E\n" EEeeee;
+
+	E ee EEeeeee EEEeeeeeeee;
+	E eee EEeeeee EEEeeeeeeeeee;
+	E eeee EEeeeee ee + eee;
+
+	e EEeee "%d\n", eeee EEeeee;
+
+	EEE EEEe;
+EEee
